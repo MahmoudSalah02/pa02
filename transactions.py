fe1c5392e42793ebe67881ee97221ae2c56473dc
f1lthyca55ual <vule@brandeis.edu>
2022-03-25 09:15:34 -0400

    Fixed some bug, as well as add a README

--- transactions.py
+++ transactions.py
@@ -86,8 +86,6 @@ class Transaction():
         ''',(rowid,))
         con.commit()
         con.close()
-<<<<<<< HEAD
-=======
 
     def sort_by_month(self,month):
         con = sqlite3.connect(self.dbfile)
@@ -126,4 +124,3 @@ class Transaction():
         con.close()
         return to_trans_dict_list(transactions)
     
->>>>>>> da0c1cbd83f2e6444a13c36218abc81d9c9ad52f
